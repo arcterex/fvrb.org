421bbb7fe8377c98787f73ebf9d0b5df97bd35d8
Miles K. Forrest <miles@coderpath.com>
2009-11-07 10:30:06 -0800

    added scaffolding for Member and Book

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :books
+
+  map.resources :members
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
