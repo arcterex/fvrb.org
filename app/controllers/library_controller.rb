a9463389a95ef4c01a91ee192665ed42ebb6ba3b
Miles K. Forrest <miles@coderpath.com>
2009-11-09 11:47:05 -0800

    added Library controller, shows list of users and their books

---
+++ app/controllers/library_controller.rb
@@ -0,0 +1,12 @@
+class LibraryController < ApplicationController
+
+  def index
+    @members = Member.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @members }
+    end
+  end
+
+end
