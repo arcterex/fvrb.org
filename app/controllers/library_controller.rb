b966faff77e89a0229443b5178f14fdcc52b9689
Alan Bailward <alan@ufies.org>
2010-05-12 20:31:05 -0700

    Upped gem requirement to 2.3.5 Only display users on the main page that have books.

--- app/controllers/library_controller.rb
+++ app/controllers/library_controller.rb
@@ -1,8 +1,9 @@
 class LibraryController < ApplicationController
 
   def index
-    @members = Member.all
-
+    # only show the members that have any books
+    @members = Member.all.select { |x| x if x.books.count > 0 }
+    
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @members }
